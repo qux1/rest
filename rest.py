642d342fa63ff2615b65b1cdb8cdf9e3e57e06f2
qux1 <118850332+qux1@users.noreply.github.com>
2025-01-27 05:25:49 +0900

    Update rest.py

--- rest.py
+++ rest.py
@@ -1,47 +1,66 @@
-from requests import *
-from user_agent import generate_user_agent
+import os, uuid, string, random
+try:
+    import requests
+except ImportError:
+    os.system("pip install requests")
+    import requests
 
-email = 'alexwashere2023@Fuck.com'
-class Rest:
-	def __init__(self):
-		
-		# Get Cookies from link rest 
-		Cookie = get("https://www.instagram.com/api/v1/web/accounts/account_recovery_send_ajax/",headers={'user-agent':str(generate_user_agent())}).cookies.get_dict()
-		
-		self.csrfToken=Cookie['csrftoken']
-		self.ig_did=Cookie['ig_did']
-		self.mid=Cookie['mid']
-		
-	#Call the rest function .	
-		self.rest()
-		
-	def rest(self):
-		
-		# Send Rest with Email 
-		Send_Requets = post("https://www.instagram.com/api/v1/web/accounts/account_recovery_send_ajax/",headers={'accept':'*/*',
-'accept-encoding':'gzip, deflate, br',
-'accept-language':'ar-AE,ar;q=0.9,en-US;q=0.8,en;q=0.7',
-'content-length':'62',
-'content-type':'application/x-www-form-urlencoded',
-'cookie':f'csrftoken={self.csrfToken}; mid={self.mid}; ig_did={self.ig_did}',
-'origin':'https://www.instagram.com',
-'referer':'https://www.instagram.com/accounts/password/reset/',
-'sec-ch-prefers-color-scheme':'light',
-'sec-ch-ua':'"Chromium";v="111",                                  "Not(A:Brand";v="8"',
-         'sec-ch-ua-mobile':'?1',
-         'sec-ch-ua-platform':'"Android"',
-         'sec-fetch-dest':'empty',
-         'sec-fetch-mode':'cors',
-         'sec-fetch-site':'same-origin',
-         'user-agent':str(generate_user_agent()),
-         'viewport-width':'412',
-         'x-asbd-id':'198387',
-         'x-csrftoken':str(self.csrfToken),
-         'x-ig-app-id':'1217981644879628',
-         'x-ig-www-claim':'0',
-         'x-instagram-ajax':'150e106c61a4',
-         'x-requested-with':'XMLHttpRequest"'},data={'email_or_username':str(email),'recaptcha_challenge_field':''})
-		if "checkpoint_required"in Send_Requets.text:
-			return True
-		else:
-			return False
+
+class Xnce():
+    def __init__(self):
+        self.target = input("[ + ] Target: ")
+        if self.target[0] == "@":
+            print("[ - ] Enter User Without '@' ", end="")
+            input()
+            exit()
+        if "@" in self.target:
+            self.data = {
+                "_csrftoken":
+                "".join(
+                    random.choices(string.ascii_lowercase +
+                                   string.ascii_uppercase + string.digits,
+                                   k=32)),
+                "user_email":
+                self.target,
+                "guid":
+                uuid.uuid4(),
+                "device_id":
+                uuid.uuid4()
+            }
+        else:
+            self.data = {
+                "_csrftoken":
+                "".join(
+                    random.choices(string.ascii_lowercase +
+                                   string.ascii_uppercase + string.digits,
+                                   k=32)),
+                "username":
+                self.target,
+                "guid":
+                uuid.uuid4(),
+                "device_id":
+                uuid.uuid4()
+            }
+        self.send_password_reset()
+
+    def send_password_reset(self):
+        head = {
+            "user-agent":
+            f"Instagram 150.0.0.0.000 Android (29/10; 300dpi; 720x1440; {''.join(random.choices(string.ascii_lowercase+string.digits, k=16))}/{''.join(random.choices(string.ascii_lowercase+string.digits, k=16))}; {''.join(random.choices(string.ascii_lowercase+string.digits, k=16))}; {''.join(random.choices(string.ascii_lowercase+string.digits, k=16))}; {''.join(random.choices(string.ascii_lowercase+string.digits, k=16))}; en_GB;)"
+        }
+        req = requests.post(
+            "https://i.instagram.com/api/v1/accounts/send_password_reset/",
+            headers=head,
+            data=self.data)
+        #print(req.text, req.status_code)
+        if "obfuscated_email" in req.text:
+            print(f"[ + ] {req.text}")
+            input()
+            exit()
+        else:
+            print(f"[ - ] {req.text}")
+            input()
+            exit()
+
+
+Xnce()
